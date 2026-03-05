3413bd9f38307acedc521453802c15b1676a735a
ather ali <a.ngaming102734@gmail.com>
2026-03-06 04:08:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// var ather= ("Ather ali");
+// console.log(ather);
+// var Sum = 10 + 20;
+// console.log(Sum);
+
+// var num1 = 10;
+// // console.log(num1++);
+// if (num1 > 100) {
+//     console.log("num1 is greater than 100");
+// 
+// var x =prompt ("Ather Ali")
+// if (x === "Ather Ali") {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+             
